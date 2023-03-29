b097a6f7d7ee3fa7ff2b49a80811321c09427a22
nizrum <sema2005@icloud.com>
2023-03-29 21:04:24 +0400

    Починил слайдер

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
-      
-const swiper = new Swiper('.swiper-container', {
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
 
     loop: true,
     slidesPerView: 1,
